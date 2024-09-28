50bc9951de3b36024a5c3ab44d3b248767d3f00d
Ahmed Kahin Nesteho <60736@etu.he2b.be>
2024-09-28 22:44:24 +0200

    Final commit

--- todo/src/main/java/restful/api/todo/exception/PermissionDeniedException.java
+++ todo/src/main/java/restful/api/todo/exception/PermissionDeniedException.java
@@ -5,7 +5,6 @@ public class PermissionDeniedException extends RuntimeException{
     public PermissionDeniedException(String message) {
         super(message);
     }
-
     public PermissionDeniedException(String message, Throwable cause) {
         super(message, cause);
     }
